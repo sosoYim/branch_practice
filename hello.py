9e88d5f97056447074b301714cf770e89e4c0502
sosoYim <65655597+sosoYim@users.noreply.github.com>
2021-07-23 12:35:38 +0900

    feat: print helloworld
    when j times 15

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-for j in range(1,10+1):
-    if j%3==0:
+for j in range(1,16+1):
+    if j%15==0:
+        print('hello world')
+    elif j%3==0:
         print('hello')
     elif j%5==0:
         print('world')
